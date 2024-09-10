d61df905fbe8d6d1ceaa1ae3d4d9fd00332a7957
erendogan6 <erendogan6@hotmail.com>
2024-09-11 00:59:06 +0300

    feat(presentation): add RandomWordsViewModel for managing word list state

---
+++ app/src/main/java/com/erendogan6/translateify/presentation/viewmodel/RandomWordsViewModel.kt
@@ -0,0 +1,47 @@
+package com.erendogan6.translateify.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.erendogan6.translateify.domain.model.Word
+import com.erendogan6.translateify.domain.usecase.GetRandomWordsUseCase
+import com.erendogan6.translateify.domain.usecase.UpdateLearnedStatusUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RandomWordsViewModel
+    @Inject
+    constructor(
+        private val getRandomWordsUseCase: GetRandomWordsUseCase,
+        private val updateLearnedStatusUseCase: UpdateLearnedStatusUseCase,
+    ) : ViewModel() {
+        private val _words = MutableStateFlow<List<Word>>(emptyList())
+        val words: StateFlow<List<Word>> = _words
+
+        init {
+            loadRandomWords()
+        }
+
+        fun loadRandomWords(shuffle: Boolean = false) {
+            viewModelScope.launch {
+                getRandomWordsUseCase().collect { wordList ->
+                    _words.value = if (shuffle) wordList.shuffled() else wordList
+                }
+            }
+        }
+
+        fun toggleLearnedStatus(word: Word) {
+            viewModelScope.launch {
+                val updatedWord = word.copy(isLearned = !word.isLearned)
+                updateLearnedStatusUseCase(updatedWord)
+
+                _words.value =
+                    _words.value.map {
+                        if (it.id == updatedWord.id) updatedWord else it
+                    }
+            }
+        }
+    }
