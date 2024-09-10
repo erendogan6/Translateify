bca9be61f93974b2a38b889963ee81191b44d1da
erendogan6 <erendogan6@hotmail.com>
2024-09-11 01:05:12 +0300

    feat(ui): add WordDetailFragment for displaying word details and actions

---
+++ app/src/main/java/com/erendogan6/translateify/presentation/ui/WordDetailFragment.kt
@@ -0,0 +1,74 @@
+package com.erendogan6.translateify.presentation.ui
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.erendogan6.translateify.R
+import com.erendogan6.translateify.databinding.FragmentWordDetailBinding
+import com.erendogan6.translateify.domain.model.Word
+import com.erendogan6.translateify.presentation.viewmodel.RandomWordsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WordDetailFragment : Fragment(R.layout.fragment_word_detail) {
+    private var _binding: FragmentWordDetailBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: RandomWordsViewModel by viewModels()
+    private var word: Word? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentWordDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        word =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                arguments?.getParcelable("word", Word::class.java)
+            } else {
+                arguments?.getParcelable("word")
+            }
+
+        word?.let { word ->
+            binding.tvWordDetail.text = word.english
+            binding.tvTranslationDetail.text = word.translation
+
+            binding.btnToggleLearned.text =
+                if (word.isLearned) {
+                    "Unlearned"
+                } else {
+                    "Learned"
+                }
+
+            binding.btnToggleLearned.setOnClickListener {
+                viewModel.toggleLearnedStatus(word)
+                requireActivity().onBackPressed()
+            }
+
+            binding.btnPronounce.setOnClickListener {
+                pronounceWord(word)
+            }
+        }
+    }
+
+    private fun pronounceWord(word: Word) {
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
