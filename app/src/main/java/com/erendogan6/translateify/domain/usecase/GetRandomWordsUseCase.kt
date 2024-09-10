d3686b8e93f0daa7d382937d284ee0945f0ba0d4
erendogan6 <erendogan6@hotmail.com>
2024-09-11 00:52:19 +0300

    feat(domain): add GetRandomWordsUseCase to retrieve random words

---
+++ app/src/main/java/com/erendogan6/translateify/domain/usecase/GetRandomWordsUseCase.kt
@@ -0,0 +1,14 @@
+package com.erendogan6.translateify.domain.usecase
+
+import com.erendogan6.translateify.domain.model.Word
+import com.erendogan6.translateify.domain.repository.WordRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetRandomWordsUseCase
+    @Inject
+    constructor(
+        private val wordRepository: WordRepository,
+    ) {
+        operator fun invoke(): Flow<List<Word>> = wordRepository.getRandomWords()
+    }
