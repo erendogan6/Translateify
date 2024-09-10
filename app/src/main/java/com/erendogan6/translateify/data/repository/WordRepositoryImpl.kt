904b1cad27a69cee2e0bb333dc296ae5061c1b06
erendogan6 <erendogan6@hotmail.com>
2024-09-11 00:27:03 +0300

    feat(data): implement WordRepository with WordRepositoryImpl

---
+++ app/src/main/java/com/erendogan6/translateify/data/repository/WordRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.erendogan6.translateify.data.repository
+
+import com.erendogan6.translateify.data.local.WordDao
+import com.erendogan6.translateify.data.mapper.toDomainModel
+import com.erendogan6.translateify.data.mapper.toEntity
+import com.erendogan6.translateify.domain.model.Word
+import com.erendogan6.translateify.domain.repository.WordRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class WordRepositoryImpl(
+    private val wordDao: WordDao,
+) : WordRepository {
+    override fun getRandomWords(): Flow<List<Word>> =
+        wordDao
+            .getRandomWords()
+            .map { entities -> entities.map { it.toDomainModel() } }
+
+    override fun getLearnedWords(): Flow<List<Word>> =
+        wordDao
+            .getLearnedWords()
+            .map { entities -> entities.map { it.toDomainModel() } }
+
+    override suspend fun updateLearnedStatus(word: Word) {
+        wordDao.updateWord(word.toEntity())
+    }
+}
