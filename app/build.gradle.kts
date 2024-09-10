7111b1784c0ba0e46286da004d12edf14c47e01a
erendogan6 <erendogan6@hotmail.com>
2024-09-11 00:18:16 +0300

    feat(build): add new plugins and update dependencies
    - Added Hilt, Kotlin KAPT, Kotlin Parcelize, and Kover plugins.
    - Updated target SDK version to 35.
    - Changed Java compatibility to version 17.
    - Enabled view binding.
    - Integrated additional dependencies: Retrofit, OkHttp, Room, Hilt, Coroutines, Gson, Lottie, Chucker, Glide, LeakCanary, Navigation, Compose, and testing libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,58 +1,134 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("org.jetbrains.kotlinx.kover")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
 }
 
 android {
-    namespace =
-        "com.erendogan6.translateify"
-    compileSdk =
-        34
+    namespace = "com.erendogan6.translateify"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId =
-            "com.erendogan6.translateify"
-        minSdk =
-            26
-        targetSdk =
-            34
-        versionCode =
-            1
-        versionName =
-            "1.0"
+        applicationId = "com.erendogan6.translateify"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
 
-        testInstrumentationRunner =
-            "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled =
-                false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro")
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
-        sourceCompatibility =
-            JavaVersion.VERSION_1_8
-        targetCompatibility =
-            JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget =
-            "1.8"
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
 dependencies {
 
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // OkHttp
+    implementation(libs.okhttp)
+    implementation(libs.loggingInterceptor)
+
+    // Room Database
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.swiperefreshlayout)
+    annotationProcessor(libs.androidx.room.compiler)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // Coroutine
+    implementation(libs.kotlinx.coroutines.android)
+
+    // JSON Parsing
+    implementation(libs.google.gson)
+
+    // Lottie
+    implementation(libs.lottie.compose)
+
+    // Chucker
+    debugImplementation(libs.library)
+    releaseImplementation(libs.library.no.op)
+
+    // Glide for Image Loading
+    implementation(libs.glide)
+    kapt(libs.compiler)
+
+    // LeakCanary
+    debugImplementation(libs.leakcanary.android)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Compose
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity.compose)
+    // implementation (platform(libs.androidx.compose.bom)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+
+    // Unit Test Dependencies
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.assertj.android)
+    testImplementation(libs.mockk.mockk)
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.jetbrains.kotlinx.coroutines.test)
+
+    // Android Instrumentation Test Dependencies
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    // androidTestImplementation (platform(libs.androidx.compose.bom)
+    androidTestImplementation(libs.mockk.android)
+
+    // Compose Test Rules
+    debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.tooling.preview)
+
+    // ViewModel and Livedata
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.fragment.ktx)
+
+    // Core
+    implementation(libs.core.ktx)
+    implementation(libs.androidx.junit.ktx)
+    implementation(libs.androidx.runner)
+    implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+}
+kapt {
+    correctErrorTypes = true
 }
