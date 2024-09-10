1dc08ee22c933fc6ad4d708d806f0c7346ccd7d1
erendogan6 <erendogan6@hotmail.com>
2024-09-11 00:24:57 +0300

    feat(domain): add WordRepository interface

---
+++ app/src/main/java/com/erendogan6/translateify/domain/repository/WordRepository.kt
@@ -0,0 +1,12 @@
+package com.erendogan6.translateify.domain.repository
+
+import com.erendogan6.translateify.domain.model.Word
+import kotlinx.coroutines.flow.Flow
+
+interface WordRepository {
+    fun getRandomWords(): Flow<List<Word>>
+
+    fun getLearnedWords(): Flow<List<Word>>
+
+    suspend fun updateLearnedStatus(word: Word)
+}
