df0b817bf63ca1efeeea6c03790d600c36737ef5
erendogan6 <erendogan6@hotmail.com>
2024-09-11 00:28:29 +0300

    feat(di): load initial data into Room database from JSON on app start

--- app/src/main/java/com/erendogan6/translateify/di/TranslateifyApp.kt
+++ app/src/main/java/com/erendogan6/translateify/di/TranslateifyApp.kt
@@ -1,7 +1,39 @@
 package com.erendogan6.translateify.di
 
 import android.app.Application
+import android.content.Context
+import com.erendogan6.translateify.data.local.AppDatabase
+import com.erendogan6.translateify.data.mapper.toEntity
+import com.erendogan6.translateify.utils.Utils
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
-class TranslateifyApp : Application()
+class TranslateifyApp : Application() {
+    @Inject
+    lateinit var database: AppDatabase
+
+    override fun onCreate() {
+        super.onCreate()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            loadInitialData(this@TranslateifyApp)
+        }
+    }
+
+    private suspend fun loadInitialData(context: Context) {
+        val wordDao = database.wordDao()
+
+        val count = wordDao.getWordsCount()
+        if (count == 0) {
+            val jsonString = Utils.loadJSONFromAsset(context, "words.json")
+            jsonString?.let {
+                val wordList = Utils.parseWordsFromJson(it)
+                wordDao.insertWords(wordList.map { it.toEntity() })
+            }
+        }
+    }
+}
