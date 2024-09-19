55b29b4b1ca89cec8ddaf581f23bcbb5629995a4
erendogan6 <erendogan6@hotmail.com>
2024-09-19 22:53:34 +0300

    feat(app): set up notification channel and initialize Timber in TranslateifyApp

--- app/src/main/java/com/erendogan6/translateify/di/TranslateifyApp.kt
+++ app/src/main/java/com/erendogan6/translateify/di/TranslateifyApp.kt
@@ -1,18 +1,31 @@
 package com.erendogan6.translateify.di
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.util.Log
+import com.erendogan6.translateify.BuildConfig
 import com.erendogan6.translateify.R
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class TranslateifyApp : Application() {
+    companion object {
+        const val CHANNEL_ID = "translateify_channel"
+    }
+
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        createNotificationChannel()
         val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
         val configSettings =
             remoteConfigSettings {
@@ -33,5 +46,22 @@ class TranslateifyApp : Application() {
                 Log.e("TranslateifyApp", "Failed to fetch Remote Config")
             }
         }
+    }
+
+    private fun createNotificationChannel() {
+        val name = "Translateify Channel"
+        val descriptionText = "Translateify Notification Channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel =
+            NotificationChannel(
+                CHANNEL_ID,
+                name,
+                importance,
+            ).apply {
+                description = descriptionText
+            }
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
