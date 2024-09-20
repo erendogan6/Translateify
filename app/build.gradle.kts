cfecca9dc7bc39c1b3d12c233bdc14acbbbe6206
erendogan6 <erendogan6@hotmail.com>
2024-09-20 03:55:40 +0300

    chore(release): update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.erendogan6.translateify"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
