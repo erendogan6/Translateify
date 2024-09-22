f0389ae89d2466017ab7f7dc314ca936b84c7aaf
erendogan6 <erendogan6@hotmail.com>
2024-09-23 01:58:04 +0300

    chore(dependencies): remove unused GlideModule

--- app/src/main/java/com/erendogan6/translateify/di/module/GlideModule.kt
+++
@@ -1,7 +0,0 @@
-package com.erendogan6.translateify.di.module
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class GlideModule : AppGlideModule()
